019565151fab2641c31dbd5333c8b3db70beb761
Uğur Kerim Sirke <robotdevre@gmail.com>
2025-04-05 00:07:08 +0300

    Create led_senkron_kontrol.ino

---
+++ led_senkron_kontrol.ino
@@ -0,0 +1,17 @@
+int led1 = 8;
+int led2 = 9;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  delay(500); // 0.5 saniye açık
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  delay(500); // 0.5 saniye kapalı
+}
